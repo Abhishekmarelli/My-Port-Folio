3844111f6a164db8eca0c44be04556034acbefbc
Abhishekmarelli <abhishek.marelli@gmail.com>
2024-05-10 16:58:58 +0530

    .....

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,15 +1,18 @@
-import React, { useRef } from 'react'
+import React, { useRef} from 'react'
 import message from '../Assets/message.png'
 import { firestore } from '../config';
 import { collection,addDoc } from 'firebase/firestore';
 import { motion } from 'framer-motion';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Contact = () => {
 	const ref = collection(firestore,'messages')
 	const uname = useRef()
 	const message_text =useRef()
 	const email = useRef()
-
+	const notify = () => toast("Your Message Sent Successfully");
 
 	const handlemessage = async(e)=>{
 		e.preventDefault();
@@ -20,6 +23,9 @@ const Contact = () => {
 		 }
 		 try{
 					addDoc(ref,data)
+					notify();
+					document.getElementById('input-form').reset();
+					
 				} 
 		 catch (error) {
 
@@ -30,6 +36,8 @@ const Contact = () => {
 	}
     return (
       <section id='Contact' className="ezy__contact8 light py-4 md:py-14 bg-[#ffffff] text-zinc-900 dark:bg-[#0b1727] dark:text-white relative overflow-hidden">
+
+			<ToastContainer/>
 			<div className="container px-4">
 			<div className="grid grid-cols-12">
 				<div className="col-span-12 lg:col-span-5 lg:col-start-2 lg:px-16 xl:px-20">
@@ -42,11 +50,13 @@ const Contact = () => {
 			<div className="mb-4">
 				<input ref={uname}
 					type="text"
+					required
 					className="min-h-[38px] leading-[38px] bg-[#F2F6FD] dark:bg-[#2A384C] border border-transparent rounded-xl focus:outline-none focus:border focus:border-[#86b7fe] w-full px-5"
 					placeholder="Enter Name"/>
 			</div>
 			<div className="mb-4">
 				<input ref={email}
+					required
 					type="email"
 					className="min-h-[38px] leading-[38px] bg-[#F2F6FD] dark:bg-[#2A384C] border border-transparent rounded-xl focus:outline-none focus:border focus:border-[#86b7fe] w-full px-5"
 					placeholder="Enter Email"
@@ -58,15 +68,16 @@ const Contact = () => {
 					className="min-h-[38px] leading-[38px] bg-[#F2F6FD] dark:bg-[#2A384C] border border-transparent rounded-xl focus:outline-none focus:border focus:border-[#86b7fe] w-full px-5"
 					placeholder="Enter Message"
 					rows="4"
-				></textarea>
+				>
+				</textarea>
 			</div>
 			<div className="text-start">
-				<motion.button
+				{<motion.button
 				whileHover={{scale:0.95, rotate:1}}
 					type="submit"
 					className="bg-blue-600 hover:bg-opacity-90 text-white px-8 py-3 rounded mb-4">
 					Submit
-				</motion.button>
+				</motion.button>}
 			</div>
 		</form>
 	</div>
